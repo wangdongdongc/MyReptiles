dae47a841d5ab4ecc0faadf7070f963fc19aaad4
wangdongdong <wddcug@gmail.com>
2018-03-02 22:10:34 +0800

    该进了 localization 模块

--- src/modules/localization.ts
+++ src/modules/localization.ts
@@ -1,11 +1,20 @@
 /**
- * 获取北京时区(UTC+8)的当前时间对象(需要计算时区偏差)
+ * 获取北京时区(UTC+8)的当前时间
  * @return {Date} 北京时间
  */
-export function getBeijingDate() {
-    const d = new Date()
-    const localTime = d.getTime()
-    const localOffset = d.getTimezoneOffset() * 60000 // 获得当地时间偏移的毫秒数
+export function getBeijingDate(): Date {
+    return toBeijingDate(new Date())
+}
+
+
+/**
+ * 将时区对象转换为取北京时区(UTC+8)的时间对象(需要计算时区偏差)
+ * @return {Date} 北京时间
+ */
+
+export function toBeijingDate(date: Date): Date {
+    const localTime = date.getTime()
+    const localOffset = date.getTimezoneOffset() * 60000 // 获得当地时间偏移的毫秒数
 
     const utc = localTime + localOffset // utc即GMT时间
     const offset = 8 // 北京(UTC+8)
@@ -18,6 +27,6 @@ export function getBeijingDate() {
  * 获取北京时区(UTC+8)的时间戳(字符串)
  * @return {string} 北京时间戳
  */
-export function getBeijingDateStamp() {
+export function getBeijingDateStamp(): string {
     return getBeijingDate().toLocaleString()
 }
